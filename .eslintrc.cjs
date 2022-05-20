2fd42e7b17be576bb9e9c7570cf04446a96fd38b
Mark Hanna <cipscis+github@gmail.com>
2022-05-20 20:55:56 +1200

    Disabled `@typescript-eslint/no-floating-promises` linting rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -69,6 +69,9 @@ module.exports = {
 			},
 		],
 
+		// There are plenty of times where it's safe to use a Promise without error handling
+		'@typescript-eslint/no-floating-promises': 'off',
+
 		////////////////////////
 		// Debugging warnings //
 		////////////////////////

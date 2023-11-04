923b2e11f85657066ad159f1ed0a0bd390876173
Mark Hanna <cipscis+github@gmail.com>
2023-11-05 12:17:34 +1300

    Migrated code style rules to eslint stylistic

--- app/assets/js/src/main.ts
+++ app/assets/js/src/main.ts
@@ -4,6 +4,6 @@ console.log('code goes here');
  * This example function exists to demonstrate how to write tests.
  * See [`main.test.ts`](./main.test.ts) for an example.
  */
-export function example(a: number, b: number) {
+export function example(a: number, b: number): number {
 	return a + b;
 }

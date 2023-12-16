ae4d2d687192223cf077e0ea4ebc3759ad83175e
Mark Hanna <cipscis@gmail.com>
2023-12-16 19:18:16 +1300

    Created Preact template

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,8 @@ module.exports = {
 	plugins: [
 		'@stylistic',
 		'@typescript-eslint',
+		'react',
+		'react-hooks',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
@@ -25,6 +27,16 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 	],
+	settings: {
+		// Copied from `eslint-config-preact` (which is too opinionated for me to use, e.g. assumes Jest globals)
+		react: {
+			// eslint-plugin-preact interprets this as "h.createElement",
+			// however we only care about marking h() as being a used variable.
+			pragma: 'h',
+			// We use "react 16.0" to avoid pushing folks to UNSAFE_ methods.
+			version: '16.0'
+		},
+	},
 	ignorePatterns: [
 		'*.md',
 		'tsconfig.json',
@@ -131,6 +143,43 @@ module.exports = {
 		'@typescript-eslint/explicit-module-boundary-types': [
 			'error'
 		],
+
+		////////////////////////
+		// Preact / JSX Rules //
+		////////////////////////
+		// Copied from `eslint-config-preact` (which is too opinionated for me to use, e.g. assumes Jest globals)
+		'react/no-deprecated': 'error',
+		// Preact disables this, but in my opinion it should be enabled
+		// It automatically picks up our configured `pragma: 'h'` configuration
+		'react/react-in-jsx-scope': 'error',
+		'react/display-name': ['warn', { ignoreTranspilerName: false }],
+		'react/jsx-no-bind': ['warn', {
+			ignoreRefs: true,
+			allowFunctions: true,
+			allowArrowFunctions: true
+		}],
+		'react/jsx-no-comment-textnodes': 'error',
+		'react/jsx-no-duplicate-props': 'error',
+		'react/jsx-no-target-blank': 'error',
+		'react/jsx-no-undef': 'error',
+		'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+		'react/jsx-uses-react': 'error',
+		'react/jsx-uses-vars': 'error',
+		'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+		'react/self-closing-comp': 'error',
+		'react/prefer-es6-class': 'error',
+		'react/prefer-stateless-function': 'warn',
+		'react/require-render-return': 'error',
+		'react/no-danger': 'warn',
+		// Legacy APIs not supported in Preact:
+		'react/no-did-mount-set-state': 'error',
+		'react/no-did-update-set-state': 'error',
+		'react/no-find-dom-node': 'error',
+		'react/no-is-mounted': 'error',
+		'react/no-string-refs': 'error',
+
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
 
 		////////////////
 		// Code style //
@@ -371,6 +420,47 @@ module.exports = {
 						after: true,
 					},
 				},
+			},
+		],
+
+		// JSX-specific code style rules
+		'@stylistic/jsx-closing-bracket-location': [
+			'error',
+			'line-aligned',
+		],
+		'@stylistic/jsx-quotes': [
+			'error',
+			'prefer-double',
+		],
+		'@stylistic/jsx-equals-spacing': [
+			'error',
+			'never',
+		],
+		'@stylistic/jsx-first-prop-new-line': [
+			'error',
+			'multiline-multiprop',
+		],
+		'@stylistic/jsx-max-props-per-line': [
+			'error',
+			{
+				maximum: 1,
+				when: 'multiline',
+			},
+		],
+		'@stylistic/jsx-self-closing-comp': [
+			'error',
+			{
+				component: true,
+				html: true,
+			},
+		],
+		'@stylistic/jsx-tag-spacing': [
+			'error',
+			{
+				'closingSlash': 'never',
+				'beforeSelfClosing': 'always',
+				'afterOpening': 'never',
+				'beforeClosing': 'allow'
 			},
 		],
 	},
